d50780b8b482471a0c541437111f443691b5c49b
Chaitra368 <ganapanenichaitrasai@gmail.com>
2025-05-20 22:10:05 +0530

    learnt react debugging.

---
+++ part1/componentState-and-EventHandlers/reactlearn1.d/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  return (
+    <div>
+      {value}
+      <button onClick={setValue(0)}>button</button>
+    </div>
+  )
+}
+
+export default App;
